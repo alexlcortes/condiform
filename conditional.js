375b62d6cb9286f642b59e138447b562ff91a6dd
Alex <alex@admins-imac.attlocal.net>
2017-01-18 10:22:00 -0500

    second commit

--- conditional.js
+++ conditional.js
@@ -1,24 +1,26 @@
 //Copyright 2015 Pareto Software, LLC, released under an MIT license: http://opensource.org/licenses/MIT
 $( document ).ready(function() {
-		var testimonial_ok=false;
+		console.log("Yes.");
+		// var testimonial_ok=false;
 		//Inputs that determine what fields to show
-		var rating = $('#live_form input:radio[name=rating]');
-		var testimonial=$('#live_form input:radio[name=testimonial]');				
+		var firstSelection = $('#live_form input:image');
+		// var testimonial=$('#live_form input:radio[name=testimonial]');				
 		
 		//Wrappers for all fields
-		var bad = $('#live_form textarea[name="feedback_bad"]').parent();
-		var ok = $('#live_form textarea[name="feedback_ok"]').parent();
-		var great = $('#live_form textarea[name="feedback_great"]').parent();
-		var testimonial_parent = $('#live_form #div_testimonial');
-		var thanks_anyway  = $('#live_form #thanks_anyway');
-		var all=bad.add(ok).add(great).add(testimonial_parent).add(thanks_anyway);
+		var secondSection = $('#live_form input[name="act1"]').parent();
+		// var ok = $('#live_form textarea[name="feedback_ok"]').parent();
+		// var great = $('#live_form textarea[name="feedback_great"]').parent();
+		// var testimonial_parent = $('#live_form #div_testimonial');
+		// var thanks_anyway  = $('#live_form #thanks_anyway');
+		// var all=bad.add(ok).add(great).add(testimonial_parent).add(thanks_anyway);
 		
-		rating.change(function(){
+		firstSelection.change(function(){
+			console.log("Extra Yes.");
 			var value=this.value;						
 			all.addClass('hidden'); //hide everything and reveal as needed
 			
-			if (value == 'vaca1' || value == 'Fair'){
-				bad.removeClass('hidden');								
+			if (value == 'vaca1'){
+				secondSection.removeClass('hidden');								
 			}
 			else if (value == 'Good' || value == 'Very Good'){
 				ok.removeClass('hidden');
@@ -31,18 +33,18 @@ $( document ).ready(function() {
 		});	
 
 		
-		testimonial.change(function(){
-			all.addClass('hidden'); 
-			testimonial_parent.removeClass('hidden');
+		// testimonial.change(function(){
+		// 	all.addClass('hidden'); 
+		// 	testimonial_parent.removeClass('hidden');
 		
-			testimonial_ok=this.value;
+		// 	testimonial_ok=this.value;
 			
-			if (testimonial_ok == 'yes'){
-				great.removeClass('hidden');
-			}
-			else{
-				thanks_anyway.removeClass('hidden');
-			}
+		// 	if (testimonial_ok == 'yes'){
+		// 		great.removeClass('hidden');
+		// 	}
+		// 	else{
+		// 		thanks_anyway.removeClass('hidden');
+		// 	}
 			
-		});
+		// });
 });
